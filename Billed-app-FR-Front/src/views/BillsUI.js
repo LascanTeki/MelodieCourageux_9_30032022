edf0b700994325beabdacf8da3a51076e64e7d9b
LascanTeki <91802117+LascanTeki@users.noreply.github.com>
2022-03-31 00:00:22 +0200

    wrong testid in Login.js

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -17,18 +17,19 @@ const row = (bill) => {
       </td>
     </tr>
     `)
-  }
+}
 
 const rows = (data) => {
+  if (data){
   data.sort(function (a, b) {
     let idA = new Date(a.date), idB = new Date(b.date)
     return idB - idA
-});
+  });}
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -51,7 +52,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
