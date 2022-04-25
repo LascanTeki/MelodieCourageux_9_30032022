a6081a817b24752ce9099b0d53c719fe0a8648e2
LascanTeki <91802117+LascanTeki@users.noreply.github.com>
2022-04-26 00:57:27 +0200

    Working on NewBill & Bill

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,15 +2,17 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH } from "../constants/routes.js";
-import { localStorageMock } from "../__mocks__/localStorage.js";
-import '@testing-library/jest-dom';
-import router from "../app/Router.js";
-import mockStore from "../__mocks__/store"
 
+ import { screen, waitFor, fireEvent } from "@testing-library/dom"
+ import BillsUI from "../views/BillsUI.js"
+ import { bills } from "../fixtures/bills.js"
+ import { localStorageMock } from "../__mocks__/localStorage.js";
+ import '@testing-library/jest-dom';
+ import router from "../app/Router.js";
+ import ContBills from "../containers/Bills.js"
+ import '@testing-library/jest-dom';
+ import userEvent from '@testing-library/user-event';
+ import { ROUTES_PATH } from "../constants/routes.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -50,31 +52,14 @@ describe("Given I am connected as an employee", () => {
       expect(contentPending).toBeTruthy()
     })
     beforeEach(() => {
-      jest.spyOn(mockStore, "bills")
-      Object.defineProperty(
-          window,
-          'localStorage',
-          { value: localStorageMock }
-      )
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee',
         email: "a@a"
       }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.appendChild(root)
-      router()
     })
     describe("When an error occurs on API", () => {
       test("fetches bills from an API and fails with 404 message error", async () => {
 
-        mockStore.bills.mockImplementationOnce(() => {
-          return {
-            list: () => {
-              return Promise.reject(new Error("Erreur 404"))
-            }
-          }
-        })
         window.onNavigate(ROUTES_PATH.Bills)
         await new Promise(process.nextTick);
         const html = BillsUI({ error: "Erreur 404" });
@@ -84,14 +69,6 @@ describe("Given I am connected as an employee", () => {
       })
 
       test("fetches messages from an API and fails with 500 message error", async () => {
-
-        mockStore.bills.mockImplementationOnce(() => {
-          return {
-            list: () => {
-              return Promise.reject(new Error("Erreur 500"))
-            }
-          }
-        })
 
         window.onNavigate(ROUTES_PATH.Bills)
         await new Promise(process.nextTick);
@@ -103,4 +80,54 @@ describe("Given I am connected as an employee", () => {
     })
   })
 })
+describe("If I click on the eye Icon", () => {
+beforeEach(() => {
+  Object.defineProperty(window, "localStorage", {
+    value: localStorageMock,
+  });
+  window.localStorage.setItem(
+    "user",
+    JSON.stringify({
+      type: "Employee",
+      email: "johndoe@email.com",
+      password: "azerty",
+      status: "connected",
+    })
+  );
+});
 
+describe("If I click on the eye Icon", () => {
+  test('Then it should display the modal', () => {
+      
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+      }))
+      document.body.innerHTML = BillsUI({ data: bills })
+      const store = null
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      
+      let eyes = screen.getAllByTestId("icon-eye");
+      eyes.forEach(async eye => {
+        
+        $.fn.modal = jest.fn();
+
+        $('#modaleFile').modal('show')
+          
+          //userEvent.click(eye)
+
+         // expect.assertions(1);
+          
+          //setTimeout(() => {
+            let modal = screen.getByTestId('modaleFile');
+            expect(modal).toHaveClass(`modal fade show`);
+           // done();
+          //});
+          
+      });
+  })
+})
+  });
--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -2,14 +2,6 @@ import { ROUTES_PATH } from '../constants/routes.js'
 import { formatDate, formatStatus } from "../app/format.js"
 import Logout from "./Logout.js"
 
-
-export function handleClickIconEye (icon) {
-  const billUrl = icon.getAttribute("data-bill-url")
-  const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
-  $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
-  $('#modaleFile').modal('show')
-}
-
 export default class ContBills{
   constructor({ document, onNavigate, store, localStorage }) {
     this.document = document
@@ -19,13 +11,20 @@ export default class ContBills{
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
     if (iconEye) iconEye.forEach(icon => {
-      icon.addEventListener('click', () => handleClickIconEye(icon))
+      icon.addEventListener('click', () => this.handleClickIconEye(icon))
     })
     new Logout({ document, localStorage, onNavigate })
   }
 
   handleClickNewBill = () => {
     this.onNavigate(ROUTES_PATH['NewBill'])
+  }
+
+  handleClickIconEye = (icon) => {
+  const billUrl = icon.getAttribute("data-bill-url")
+  const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
+  $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
+  jQuery('#modaleFile').modal('show')
   }
 
   getBills = () => {
